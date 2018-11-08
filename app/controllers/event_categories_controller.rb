a9c0f3ab6965fdb7620bafdc3e65dc6f54250b8b
Damir Zekić <damirz@gmail.com>
2018-11-08 12:08:15 +0200

    Prevent search of institutions by an empty string

--- app/controllers/event_categories_controller.rb
+++ app/controllers/event_categories_controller.rb
@@ -4,6 +4,10 @@
 class EventCategoriesController < ApplicationController
   def index
     institution_id = params.fetch(:institution_id).strip
+    unless institution_id.present?
+      redirect_to root_url
+      return
+    end
 
     @institution = FetchInstitutionDetails.new.call(institution_id: institution_id)
     @event_categories = FetchEventCategories.new.call(institution: @institution)
