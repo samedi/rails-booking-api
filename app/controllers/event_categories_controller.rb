2f962734e9f395e0c97dc616d9d43929873d7237
Damir Zekić <damirz@gmail.com>
2018-11-08 12:05:44 +0200

    Strip the whitespace from the form input

--- app/controllers/event_categories_controller.rb
+++ app/controllers/event_categories_controller.rb
@@ -3,7 +3,7 @@
 # Provides actions related to event categories.
 class EventCategoriesController < ApplicationController
   def index
-    institution_id = params.fetch(:institution_id)
+    institution_id = params.fetch(:institution_id).strip
 
     @institution = FetchInstitutionDetails.new.call(institution_id: institution_id)
     @event_categories = FetchEventCategories.new.call(institution: @institution)
